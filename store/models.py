b538da7afdad11eb912d6e25ea13d91256e7ecb1
Jorge Silva <jsilvajr1@yahoo.com>
2020-07-09 15:13:53 -0500

    fixed non rendering images in store.html

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Create your models here.
+
 class Customer(models.Model):
 	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
@@ -8,6 +10,8 @@ class Customer(models.Model):
 
 	def __str__(self):
 		return self.name
+
+
 class Product(models.Model):
 	name = models.CharField(max_length=200)
 	price = models.FloatField()
@@ -17,14 +21,13 @@ class Product(models.Model):
 	def __str__(self):
 		return self.name
 
-@property
-def imageURL(self):
-    try:
-        url = self.image.url
-    except:
-        url = ''
-    return url
-
+	@property
+	def imageURL(self):
+		try:
+			url = self.image.url
+		except:
+			url = ''
+		return url
 
 class Order(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -35,11 +38,28 @@ class Order(models.Model):
 	def __str__(self):
 		return str(self.id)
 
+	@property
+	def get_cart_total(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.get_total for item in orderitems])
+		return total 
+
+	@property
+	def get_cart_items(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.quantity for item in orderitems])
+		return total 
+
 class OrderItem(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
+
+	@property
+	def get_total(self):
+		total = self.product.price * self.quantity
+		return total
 
 class ShippingAddress(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
