5636df28943d2411b84c47bfca855ae4c6ef6d71
Jorge Silva <jsilvajr1@yahoo.com>
2020-07-09 16:48:46 -0500

    added token and post request

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import * 
+from django.http import JsonResponse
 
 def store(request):
 	products = Product.objects.all()
@@ -32,3 +33,7 @@ def checkout(request):
 
 	context = {'items':items, 'order':order}
 	return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+	return JsonResponse('Item was added', safe=False)
