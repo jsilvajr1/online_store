dac29949e6b0341f9f42353a89796f648383f578
Jorge Silva <jsilvajr1@yahoo.com>
2020-07-17 10:38:40 -0500

    added guest checkout logic

--- store/views.py
+++ store/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import * 
 from django.http import JsonResponse
 import json
+import datetime
+from .models import * 
 
 def store(request):
 
@@ -13,15 +14,11 @@ def store(request):
 	else:
 		#Create empty cart for now for non-logged in user
 		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
+		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
 		cartItems = order['get_cart_items']
 
 	products = Product.objects.all()
 	context = {'products':products, 'cartItems':cartItems}
-	return render(request, 'store/store.html', context)
-
-	products = Product.objects.all()
-	context = {'products':products}
 	return render(request, 'store/store.html', context)
 
 def cart(request):
@@ -34,7 +31,7 @@ def cart(request):
 	else:
 		#Create empty cart for now for non-logged in user
 		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
+		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
 		cartItems = order['get_cart_items']
 
 	context = {'items':items, 'order':order, 'cartItems':cartItems}
@@ -49,12 +46,11 @@ def checkout(request):
 	else:
 		#Create empty cart for now for non-logged in user
 		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
+		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
 		cartItems = order['get_cart_items']
 
-	context = {'items':items, 'order':order,'cartItems':cartItems}
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/checkout.html', context)
-
 
 def updateItem(request):
 	data = json.loads(request.body)
@@ -80,3 +76,21 @@ def updateItem(request):
 		orderItem.delete()
 
 	return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		total = float(data['form']['total'])
+		order.transaction_id = transaction_id
+
+		if total == order.get_cart_total:
+			order.complete = True
+		order.save()
+	else:
+		print('User is not logged in')
+
+	return JsonResponse('Payment submitted..', safe=False)
